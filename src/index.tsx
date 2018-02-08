e74196548ec3bee45c51aec35ff76ff4a6e20f49
miguel.chauca@almundo.com <miguel.chauca@almundo.com>
2018-02-08 19:32:10 -0300

    configuracion de component

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import * as style from './header.css';
+
+export default ()=>{
+    return(
+        <h1 className={style.miguel}>
+            Hola Miguel
+        </h1>
+    )
+}
