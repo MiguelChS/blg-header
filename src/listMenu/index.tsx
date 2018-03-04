3ad0d01787350bcd854093d60f6d97829937c8a3
miguel.chauca@almundo.com <miguel.chauca@almundo.com>
2018-03-04 20:24:43 -0300

    modificando link de header

--- src/listMenu/index.tsx
+++ src/listMenu/index.tsx
@@ -5,9 +5,7 @@ import {listMenu} from './index.css';
 export const Menu = () => {
     return (
         <ul className={`${listMenu} nav navbar-nav text-uppercase pull-left`}>
-            <li><a href="archive-cat.html">Home</a></li>
-            <li><a href="about-me.html">About me</a></li>
-            <li><a href="contact.html">Contact</a></li>
+            <li><a href="/">Home</a></li>
         </ul>
     )
 }
